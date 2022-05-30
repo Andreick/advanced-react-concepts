10c456d58501c8d6bf205d12e7e817db5c98319e
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-30 01:01:51 -0300

    Using React Router

--- src/App.js
+++ src/App.js
@@ -7,9 +7,37 @@ import NewUserInfo from './NewUserInfo';
 import Layout from './Layout';
 import MainContent from './MainContent';
 import UserList from './UserList';
+import { BrowserRouter, Routes, Link, Route, NavLink } from 'react-router-dom';
+import ProductPage from './pages/ProductsPage';
+import AboutPage from './pages/AboutPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <UserList />;
+  return (
+    <BrowserRouter>
+      <div className="pages">
+        <div className="page-header">
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>{' '}
+          <NavLink to="/products" activeClassName="active">
+            Products
+          </NavLink>{' '}
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </div>
+        <div className="page-content">
+          <Routes>
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/products" element={<ProductPage />} />
+            <Route path="/products/:id" element={<ProductPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
